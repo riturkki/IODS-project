9474220625e20c232f1c826aee209fc8a72cdeb8
Riku Turkki <riturkki@gmail.com>
2017-02-24 12:20:36 +0200

    did 5th excercise

--- create_human.R
+++ create_human.R
@@ -1,5 +1,7 @@
 library(data.table)
 library(dplyr)
+library(stringr)
+
 
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
